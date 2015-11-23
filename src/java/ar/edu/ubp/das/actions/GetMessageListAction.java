1636bd26411f8964b271d2e4935f8b6ed18b0df9
Joaquin Navarro <jnfebo@gmail.com>
2015-11-22 21:58:15 -0300

    Change how messages shows to admin

--- src/java/ar/edu/ubp/das/actions/GetMessageListAction.java
+++ src/java/ar/edu/ubp/das/actions/GetMessageListAction.java
@@ -2,6 +2,7 @@ package ar.edu.ubp.das.actions;
 
 import ar.edu.ubp.das.entities.MessageEntity;
 import ar.edu.ubp.das.entities.UserAccessEntity;
+import ar.edu.ubp.das.entities.UserLoginEntity;
 import ar.edu.ubp.das.mvc.actions.Action;
 import java.util.LinkedList;
 import java.util.List;
@@ -29,46 +30,49 @@ public class GetMessageListAction extends Action{
         String roomId = (String) this.getForm().getItem("roomId");
         String roomType = (String) this.getForm().getItem("roomType");
         String profileType = (String) this.getForm().getItem("profileType");
-        String userAccessId = (String) this.getForm().getItem("userAccessId");
+        String profileId = (String) this.getForm().getItem("profileId");
         
         Client client = ClientBuilder.newClient();
         
         Form form = new Form();
-        form.param("id", userAccessId);
-        WebTarget userAccesTarget = client.target("http://localhost:8080/chat/webresources/useraccess/find/id");        
-        Invocation userAccesInvocation = userAccesTarget.request().buildPost(Entity.form(form));
-        Response userAccesResponse = userAccesInvocation.invoke();
-        UserAccessEntity userAcces = new UserAccessEntity();
+        form.param("id", profileId);
+        /**Get last login profile only for admin*/
+        WebTarget userLoginTarget = client.target("http://localhost:8080/chat/webresources/userslogins/lastlogin/profile/id");        
+        Invocation userLoginInvocation = userLoginTarget.request().buildPost(Entity.form(form));
+        Response userLoginResponse = userLoginInvocation.invoke();
+        UserLoginEntity userLogin = new UserLoginEntity();
         
-        if(userAccesResponse.getStatusInfo().getReasonPhrase().equals("OK")){
-            userAcces = userAccesResponse.readEntity(new GenericType<UserAccessEntity>(){});
-        }
-        
-        Form form1 = new Form();
-        form1.param("id", roomId);
-        WebTarget messageTarget = client.target("http://localhost:8080/chat/webresources/messages/room/id");        
-        Invocation messageInvocation = messageTarget.request().buildPost(Entity.form(form1));
-        Response messageResponse = messageInvocation.invoke();
-        
-        List<MessageEntity> messageList = messageResponse.readEntity(new GenericType<List<MessageEntity>>(){});
-        List<MessageEntity> finalMessageList = new LinkedList<>();
-        
-        if(messageList != null){
-            if(!profileType.equals("ADMIN")){
-                for(MessageEntity m : messageList){
-                    if(m.getDatetimeOfCreation().after(userAcces.getDatetimeOfAccessStart())){
-                        finalMessageList.add(m);
-                    }
+        if(userLoginResponse.getStatusInfo().getReasonPhrase().equals("OK")){
+            userLogin = userLoginResponse.readEntity(new GenericType<UserLoginEntity>(){});
+                
+            Form form1 = new Form();
+            form1.param("id", roomId);
+            /**Get room's messages*/
+            WebTarget messageTarget = client.target("http://localhost:8080/chat/webresources/messages/room/id");        
+            Invocation messageInvocation = messageTarget.request().buildPost(Entity.form(form1));
+            Response messageResponse = messageInvocation.invoke();
+
+            List<MessageEntity> messageList = messageResponse.readEntity(new GenericType<List<MessageEntity>>(){});
+            List<MessageEntity> finalMessageList = new LinkedList<>();
+
+            if(messageList != null){
+                if(profileType.equals("ADMIN")){
+                    for(MessageEntity m : messageList){
+                        if(m.getDatetimeOfCreation().after(userLogin.getDatetimeOfAccessStart())){
+                            finalMessageList.add(m);
+                        }
+                    }                 
+                    /**Send final list*/
+                    this.getForm().setItem("messageList", finalMessageList);
+                }else{
+                    /**Send empty list*/
+                    this.getForm().setItem("messageList", finalMessageList);
                 }
-                this.getForm().setItem("messageList", finalMessageList);
-            }else{
-                this.getForm().setItem("messageList", messageList);
+
+                this.getForm().setItem("profileType", profileType);
+                this.getForm().setItem("roomType", roomType);
+                this.gotoPage("/template/user/messageList.jsp", request, response);
             }
-            
-            this.getForm().setItem("profileType", profileType);
-            this.getForm().setItem("roomType", roomType);
-            this.gotoPage("/template/user/messageList.jsp", request, response);
         }
     }
-    
 }
